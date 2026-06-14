cb3939a9d02678ab8d46a0d8440360f2b809ae13
ellias-dev <info@ellias.dev>
2026-06-14 21:35:34 +0400

    connect the form to the server and send a plain response

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const path = require('path');
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
+app.post('/usersend', (req, res) => {
+    const name = req.body.yourName, meal = req.body.meal, season = req.body.season;
+    res.send(`The answer is ${name}, ${meal}, and ${season}`);
+
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
