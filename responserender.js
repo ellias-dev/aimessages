22d89197d1702067d49cf99e948452f184a14f40
ellias-dev <info@ellias.dev>
2026-06-17 14:39:06 +0400

    add basic styling

--- responserender.js
+++ responserender.js
@@ -5,11 +5,16 @@ const responseRender = (arg) => {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="src/style.css">
-    <title>Welcome to a poetry cave</title>
+    <title>Arigato!</title>
 </head>
 <body>
-    <h1>Here is your very own hokku</h1>
-    <p>${arg}</p>
+    <h1>Arigato! Your hokku is ready</h1>
+    <div class="rhyme">
+    <p class="response">${arg}</p>
+    <p class="cite"> &copy; Mr. GPT, a poet and philanthrope</p>
+    <a href="/" class="button btn">Make another one</a>
+    </div>
+    
     
 </body>
 </html>`;
